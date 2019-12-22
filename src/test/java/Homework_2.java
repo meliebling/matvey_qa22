d0c7220da2374aec63945c16f2ea56e2bf861bf6
Matvey <meliebling@users.noreply.github.com>
2019-12-22 16:31:17 +0200

    added Menu Titles and comparison

--- src/test/java/Homework_2.java
+++ src/test/java/Homework_2.java
@@ -1,11 +1,13 @@
 import org.testng.annotations.Test;
 
-public class Homework_2 extends TestBase{
+public class Homework_2 extends TestBase {
 
 
     @Test
     public void findTelRanTitles() throws InterruptedException {
         openSite("https://www.tel-ran.com/");
+
+
         getTextAboutUs();
         getTextCourses();
         getTextEmployment();
